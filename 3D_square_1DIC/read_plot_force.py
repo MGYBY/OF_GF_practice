eb429943ecaf9ff36b98a8e2788ac4d38466311e
BYY <53412286+MGYBY@users.noreply.github.com>
2021-11-10 16:15:03 -0500

    Use FluidFoam for reading/plotting forces

---
+++ 3D_square_1DIC/read_plot_force.py
@@ -0,0 +1,20 @@
+# import readforce function from fluidfoam package
+from fluidfoam.readpostpro import readforce
+import matplotlib.pyplot as plt
+
+# sol = './postProcessing/forces/0/force_0.dat'
+sol = './'
+
+force = readforce(sol, time_name = '0', name='force_0')
+
+plt.figure()
+
+plt.plot(force[:, 0], force[:, 1])
+
+# Setting axis labels
+plt.xlabel('t (s)')
+plt.ylabel('force')
+
+# add grid
+plt.grid()
+plt.show()
