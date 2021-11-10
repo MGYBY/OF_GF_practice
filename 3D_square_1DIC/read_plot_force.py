ed7588e527973b24ac6c05d29dbe5f51f2e3ea23
BYY <53412286+MGYBY@users.noreply.github.com>
2021-11-10 16:56:24 -0500

    Update read_plot_force.py

--- 3D_square_1DIC/read_plot_force.py
+++ 3D_square_1DIC/read_plot_force.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 # sol = './postProcessing/forces/0/force_0.dat'
 sol = './'
 
-force = readforce(sol, time_name = '0', name='force_0')
+# force = readforce(sol, time_name = '0', name='force_0')
+force = readforce(sol, time_name = '0', name='force')
 
 plt.figure()
 
